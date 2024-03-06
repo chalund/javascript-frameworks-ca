4ef79f294e0b813e2c28e9bcda64109e65b84b49
chalund <114383740+chalund@users.noreply.github.com>
2024-03-06 10:38:28 +0100

    adds Routing Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { Route, Routes} from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { Home, Product, Contact } from "./Pages";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
